3c61f2ed79d497626d3b87da4058bb21c5099303
IshaanKapoor <ishaankapoor2506@gmail.com>
2022-01-14 21:45:38 +0530

    Added a Refined Version

--- refined.py
+++ refined.py
@@ -1,57 +1,54 @@
 from os import getcwd
 from random import randrange
-from re import finditer, sub
-from subprocess import PIPE, run
+from re import finditer
+from subprocess import run
 
 
-class RepObj:
-    def __init__(self, replace_by, every):
-        self.__counter = 1
-        self.__every = every
-        self.__replace_by = replace_by
-
-    def doit(self, m):
-        rval = m.group(1) if self.__counter % self.__every else self.__replace_by
-        self.__counter += 1
-        return rval
-
-
-def func():
-    n = len(data)
-    result = list(data)
-    for i in range(randrange(n)):
-        result.insert(i + randrange(n), "^")
-    return "".join(result)
-
-
-temp = "sample.bat"  # input("Path of the Batch File >\t").strip()
+temp = input("Path of the Batch File >\t").strip()  # "sample.bat"
 with open(temp) as temp:
     data = temp.read()
-# temp = "would you like to add random '^' to the obfuscation (may break script. not recommended) y or n (default n):\t"
-# data = func() if input(temp).strip().lower().startswith("y") else data
-del temp
+temp = "would you like to add random '^' to the obfuscation (may break script. not recommended) y or n (default n):\t"
+
+if input(temp).strip().lower().startswith("y"):
+    n = len(data)
+    temp = list(data)
+    for i in range(randrange(n)):
+        temp.insert(i + randrange(n), "^")
+    data = "".join(temp)
 
 data += '\nset a = %%~i\nset a = % + %~i"%\nset a = %a%\n:aaaaaaaaaaaaaaaaaaaaaaaaaaaaab\n'
 data = data.replace("%%~", "ckoco").replace("%~", "croco")
-data = sub("(%)", RepObj("replaced", 2).doit, data)
+
+data = list(data)
+temp = True
+for index, item in enumerate(data):
+    if item == "%":
+        temp = not temp
+        if temp:
+            data[index] = "replaced"
+del index, item
+data = "".join(data)
+
 res = [i.start() for i in finditer("%", data)]
 temp = [i.start() for i in finditer("replaced", data)]
 result = [(res[i], temp[i] + 8) for i in range(len(temp))]
 del res
 temp = [i.start() for i in finditer("croco", data)]
-result += [(i, i+7) for i in temp]
+result += [(i, i + 7) for i in temp]
 temp = [i.start() for i in finditer("ckoco", data)]
-result += [(i, i+7) for i in temp]
+result += [(i, i + 7) for i in temp]
 temp = ""
 for i in data.splitlines():
-    if i.startswith(":"): i += "\u044f"
+    if i.startswith(":"):
+        i += "\u044f"
     temp += i + "\n"
 res = [i for i, j in enumerate(temp) if j.endswith("\u044f")]
 
 j = -1
 res1 = []
 for i in temp.splitlines():
-    if not i.startswith(":"): continue
+    if not i.startswith(":"):
+        continue
     j += 1
     res1.append(res[j] - len(i) + 1)
 del j, temp
@@ -60,11 +57,9 @@ for r in (res, res1):
     for index, item in enumerate(r[1:], start=1):
         r[index] = item - index
 result += [(res1[i], res[i]) for i in range(len(res))]
-del res, res1
+del r, res, res1
 result.sort()
 result.insert(0, (0, 0))
-result2 = [(result[i][1], result[i+1][0]) for i in range(len(result)-1)]
-result2.append((result[-1][1], int("9"*20)))
 temp = (
     ("J", 0),
     ("g", 1),
@@ -85,17 +80,18 @@ temp = (
     ("O", 17),
     ("A", 18),
 )
-staticnum = 0
-for i in range(len(result2)):
-    start = int(result2[i][0]) + staticnum
-    end = int(result2[i][1]) + staticnum
+offset = 0
+n = len(result) - 1
+for i in range(n + 1):
+    start = result[i][1] + offset
+    end = (result[i + 1][0] + offset) if i < n else len(data)
     tmp = data[start:end]
-    test2tmp = len(tmp)
+    tmp2 = len(tmp)
     for j, k in temp:
         tmp = tmp.replace(j, f"%r:~{k},1%")
     data = data[:start] + tmp + data[end:]
-    staticnum += len(tmp) - test2tmp
-del result2, staticnum, start, end, test2tmp, tmp
+    offset += len(tmp) - tmp2
+del end, n, offset, result, start, tmp2, tmp
 temp = (("ckoco", "%%~"), ("croco", "%~"), ("replaced", "%"))
 for i, j in temp:
     data = data.replace(i, j)
@@ -117,16 +113,17 @@ temp = (
     # "%public:~14,1%l%public:~4,1%" # "cLs" # "cls"
     "@^e^c%r:~15,1%^%r:~17,1% ^%r:~17,1%n",
     # "@ec%r:~15,1%%r:~17,1% %r:~17,1%n" # "@ecHO On" # "@echo on"
-    data,
+    data[:-123],
 )
+del data
 temp = "\n".join(temp)
-name = "o"  # input("enter output file name (do not include extention): ")
+name = input("enter output file name (do not include extention): ")  # "o"
 with open(name + ".bat", "w") as f:
     f.write(temp)
 temp = (
     rf'>"temp.~b64" echo(//4mY2xzDQo= && certutil.exe -f -decode "temp.~b64" "{name}tmp.bat" && del "temp.~b64" && copy "{name}tmp.bat" /b + "{getcwd()}\{name}.bat" /b',
     f"del {name}.bat /f && rename {name}tmp.bat {name}.bat",
 )
-# for i in temp:
-#     run(i, shell=True, capture_output=True, stdin=PIPE)
+for i in temp:
+    run(i, shell=True, capture_output=True, stdin=-1)
 print(f'saved to "{name}.bat"')
